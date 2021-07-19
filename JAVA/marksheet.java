74ec625dddb8d579627e07a7ac9e9c670ea1ae8a
Chandra Prakash <scp.thedevil@gmail.com>
2021-07-19 19:00:51 +0530

    User-input exercise

---
+++ JAVA/marksheet.java
@@ -0,0 +1,72 @@
+import java.util.*;
+
+class zaynmarks {
+    String name;
+    int lang2, eng2, cpp, dsa, cpplab, dsalab, maths2, total_marks;
+
+    void total() {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter your Name: ");
+        name = scn.nextLine();
+        System.out.println("Enter your marks for Subjects below:");
+        System.out.println("**************************************************");
+        System.out.println("Enter your Language marks for Subject: ");
+        lang2 = scn.nextInt();
+        System.out.println("Enter your English marks for Subject: ");
+        eng2 = scn.nextInt();
+        System.out.println("Enter your C++ marks for Subject: ");
+        cpp = scn.nextInt();
+        System.out.println("Enter your DSA marks for Subject: ");
+        dsa = scn.nextInt();
+        System.out.println("Enter your CPP Lab marks for Subject: ");
+        cpplab = scn.nextInt();
+        System.out.println("Enter your DSA Lab marks for Subject: ");
+        dsalab = scn.nextInt();
+        System.out.println("Enter your Maths-II marks for Subject: ");
+        maths2 = scn.nextInt();
+        total_marks = lang2 + eng2 + cpp + dsa + cpplab + dsalab + maths2;
+        System.out.println(" The total Marks of " + name + " is : " + total_marks);
+
+    }
+}
+
+class harrymarks {
+    String frnd;
+    int lang2, eng2, cpp, dsa, cpplab, dsalab, maths2, total_marks;
+
+    void total() {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter your Friend name: ");
+        frnd = scn.next();
+        System.out.println("Enter " + frnd + "'s marks for Subjects below:");
+        System.out.println("**************************************************");
+        System.out.println("Enter " + frnd + "'s Language marks for Subject: ");
+        lang2 = scn.nextInt();
+        System.out.println("Enter " + frnd + "'s English marks for Subject: ");
+        eng2 = scn.nextInt();
+        System.out.println("Enter " + frnd + "'s C++ marks for Subject: ");
+        cpp = scn.nextInt();
+        System.out.println("Enter " + frnd + "'s DSA marks for Subject: ");
+        dsa = scn.nextInt();
+        System.out.println("Enter " + frnd + "'s C++ Lab marks for Subject: ");
+        cpplab = scn.nextInt();
+        System.out.println("Enter " + frnd + "'s DSA Lab marks for Subject: ");
+        dsalab = scn.nextInt();
+        System.out.println("Enter " + frnd + "'s Maths2 marks for Subject: ");
+        maths2 = scn.nextInt();
+        total_marks = lang2 + eng2 + cpp + dsa + cpplab + dsalab + maths2;
+        System.out.println("The Total Marks of " + frnd + " is : " + total_marks);
+    }
+}
+
+public class marksheet {
+    public static void main(String args[])
+
+    {
+        zaynmarks obj = new zaynmarks();
+        obj.total();
+        harrymarks obj1 = new harrymarks();
+        obj1.total();
+    }
+
+}
